08d4996e9d577d720770d4fe08bbae4f618fd990
Abhipsa <abhipsa12345patnaik@gmail.com>
2026-03-04 00:12:01 +0530

    Initial Dashboard Page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 function Dashboard(){
+    const navigate=useNavigate();
+    
+    const {user,logout}=useAuth();
+    const handleLogout= ()=>{
+        try{
+            logout();
+            navigate('/login');
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
     return(
-        <h1>Dashboard</h1>
+        <>
+        <h3>Welcome {user.name}</h3>
+        <button onClick={handleLogout}>Logout</button>
+        </>
     );
 }
 
