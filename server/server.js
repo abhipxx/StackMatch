dcc73d39e0b4b683df4c568caec97086c976c8c3
Abhipsa <abhipsa12345patnaik@gmail.com>
2026-03-12 20:10:54 +0530

    v1 d

--- server/server.js
+++ server/server.js
@@ -4,7 +4,13 @@ const cors=require('cors');
 require('dotenv').config();
 
 const app=express();
-app.use(cors());
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://stack-match-nine.vercel.app/'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');const projectRoutes=require('./routes/projectRoutes');
